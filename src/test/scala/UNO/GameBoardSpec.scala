f97e32a0fc814c40ce86add81edd805e8afe721e
sonirat <Soniesen.Ratnasingam@htwg-konstanz.de>
2020-11-05 14:09:38 +0100

    fixed folder structure

---
+++ src/test/scala/UNO/GameBoardSpec.scala
@@ -0,0 +1,8 @@
+package UNO
+
+class GameBoardSpec {
+
+
+    // only for folder structuer
+    // another commit test
+}
--- src/test/scala/gameboard.scala
+++
@@ -1,6 +0,0 @@
-class gameboard {
-
-  // only for folder structuer
-  // another commit test
-
-}
