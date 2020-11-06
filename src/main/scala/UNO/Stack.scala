da4520321227eebb4d94b637776032829b68f559
sonirat <Soniesen.Ratnasingam@htwg-konstanz.de>
2020-11-06 03:01:41 +0100

    added new test class

--- src/main/scala/UNO/Stack.scala
+++ src/main/scala/UNO/Stack.scala
@@ -1,5 +1,21 @@
 package UNO
 
-class Stack {
+import scala.util.Random
+
+object Stack{
+  val initialize: List[Card] = {
+    //Card yellow = new Card(1,"yellow")
+    val green = List.fill(5)(new Card(5,"green"))
+    val yellow = List.fill(5)(new Card(5,"yellow"))
+    val blue = List.fill(5)(new Card(5,"blue"))
+    val red = List.fill(5)(new Card(5,"red"))
+
+    green ::: yellow ::: blue ::: red
+  }
+
+  def shuffleCards(a:List[Card]): List[Card] = {
+    Random.shuffle(a);
+  }
+
 
 }
