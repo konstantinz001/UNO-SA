f67032fb326fd24a942130634e68514a884377b3
konstantinz001 <konsi.zabaznov@web.de>
2021-01-15 00:46:53 +0100

    updating GUI

--- src/main/scala/UNO/controller/controllerComponent/controllerBaseImp/controller.scala
+++ src/main/scala/UNO/controller/controllerComponent/controllerBaseImp/controller.scala
@@ -1,7 +1,10 @@
 package UNO.controller.controllerComponent.controllerBaseImp
 
+
+
+import UNO.UnoGame.controller
 import UNO.UnoGameModule
-import UNO.aview.gui.SwingGui
+import UNO.aview.gui.{NameGui, SwingGui}
 import UNO.controller.GameStatus._
 import UNO.controller.controllerComponent._
 import UNO.model.GameState
@@ -20,8 +23,9 @@ import scala.swing.Publisher
 class controller @Inject() extends controllerInterface with Publisher{
 
   var gameStatus: GameStatus = IDLE
-  var playername1 = "1"
-  var playername2 = "2"
+  var playername1 = ""
+  var playername2 = ""
+  initPlayerName()
   var stackCard = initStackCard()
   var playerList = initPlayerList()
   var playStack2 = initPlayStack()
@@ -34,6 +38,7 @@ class controller @Inject() extends controllerInterface with Publisher{
   val injector = Guice.createInjector(new UnoGameModule)
   val fileIo = injector.instance[FileIOTrait]
   publish(new welcomeStates)
+  gui.open()
 
   def setDefault(): Unit = {
     stackCard = initStackCard()
@@ -79,6 +84,15 @@ class controller @Inject() extends controllerInterface with Publisher{
       starthand.init.reverse
     }
     List(Player(playername1,startHand()),Player(playername2,startHand()))
+  }
+
+  def initPlayerName(): Unit = {
+    val gui1 = new NameGui(this)
+
+    while (playername1.length == 0 && playername2.length == 0) {
+      gui1.open()
+    }
+    gui1.close()
   }
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////
