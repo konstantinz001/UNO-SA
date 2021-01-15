abab52619505127f844757ecaab516096281e996
konstantinz001 <konsi.zabaznov@web.de>
2021-01-15 10:24:08 +0100

    Task12 into Task13

---
+++ src/main/scala/UNO/model/createGame.scala
@@ -0,0 +1,27 @@
+package UNO.model
+
+import UNO.UnoGame.controller
+import UNO.model.PlayerComponent.playerBaseImp.Player
+import UNO.model.cardComponent.cardBaseImp.Card
+import UNO.model.stackComponent.stackBaseImp.Stack
+
+object createGame {
+
+  def initStackCard() : Stack = {
+    var stackCards =Stack(List(new Card("",""))).initStack()
+    /*(1 to 100).foreach((i)=>{
+      stackCard = stackCard.shuffleCards()
+    })*/
+    stackCards
+  }
+
+
+  def initPlayStack() : List[Card] = {
+
+    while (controller.stackCard.getCardFromStack().color == "black") {
+      controller.stackCard = controller.stackCard.pullCards(List(controller.stackCard.getCardFromStack()))
+      controller.stackCard = controller.stackCard.removeCard()
+    }
+    List(controller.stackCard.getCardFromStack())
+  }
+}
