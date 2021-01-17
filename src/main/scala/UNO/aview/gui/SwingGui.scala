0fc80cb0510e3b9962641312852e40e4bb07e4d0
konstantinz001 <konsi.zabaznov@web.de>
2021-01-17 23:33:54 +0100

    GUI + delete return and unused imports

--- src/main/scala/UNO/aview/gui/SwingGui.scala
+++ src/main/scala/UNO/aview/gui/SwingGui.scala
@@ -1,7 +1,7 @@
 package UNO.aview.gui
 
 
-import UNO.controller.controllerComponent.controllerBaseImp.{NameStates, updateStates}
+import UNO.controller.controllerComponent.controllerBaseImp.{updateStates}
 import UNO.controller.controllerComponent.controllerInterface
 
 import java.awt.Image
@@ -217,7 +217,6 @@ class SwingGui(controller: controllerInterface) extends Frame {
       listenTo(yesButton, noButton)
       reactions += {
         case ButtonClicked(`yesButton`) => {
-          close()
           controller.setDefault()
         }
         case ButtonClicked(`noButton`) => {
@@ -305,7 +304,6 @@ class SwingGui(controller: controllerInterface) extends Frame {
 
   reactions += {
     case a: updateStates => redraw
-    case b: NameStates => redraw3
 
   }
   visible = true
