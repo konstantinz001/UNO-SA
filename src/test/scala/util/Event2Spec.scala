db10eca60d62fc5c4117db7d1a3fde49dba2f7e5
konstantinz001 <konsi.zabaznov@web.de>
2021-01-21 17:50:39 +0100

    ENV-Var

--- src/test/scala/util/Event2Spec.scala
+++ src/test/scala/util/Event2Spec.scala
@@ -5,12 +5,12 @@ import UNO.util.removeCardEvent
 import org.scalatest.{Matchers, WordSpec}
 
 class Event2Spec extends WordSpec with Matchers {
+  controller
   "Event2" should {
     "removeCard" in {
       removeCardEvent should be (removeCardEvent)
       (controller.playerList(0).playerCards(0).color == controller.playStack2(0).color ||
-        controller.playerList(0).playerCards(0).value == controller.playStack2(0).value ||
-        controller.playerList(0).playerCards(0).color == "black") should be (false)
+        controller.playerList(0).playerCards(0).value == controller.playStack2(0).value ||) should be (true)
     }
   }
 }
