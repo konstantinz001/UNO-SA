be0bd63b1c9a271efbc89eeb14a073084ec0be0d
konstantinz001 <konsi.zabaznov@web.de>
2021-01-22 22:00:42 +0100

    Tests

--- src/test/scala/controller/removeCommandSpec.scala
+++ src/test/scala/controller/removeCommandSpec.scala
@@ -2,12 +2,15 @@ package controller
 
 import UNO.UnoGame.controller
 import UNO.controller.controllerComponent.controllerBaseImp.RemoveCommand
+import UNO.model.cardComponent.cardBaseImp.Card
+import UNO.model.stackComponent.stackBaseImp.Stack
 import UNO.util.UndoManager
 import org.scalatest.{Matchers, WordSpec}
 
 class removeCommandSpec extends WordSpec with Matchers {
+  val undoManager = new UndoManager
+  controller.playStack2 = List(Card("",""))
   "RemoveCommand" should {
-    val undoManager = new UndoManager
     "doStep" in {
       undoManager.doStep(new RemoveCommand(0, controller))
       controller.playStack2.size should be(2)
