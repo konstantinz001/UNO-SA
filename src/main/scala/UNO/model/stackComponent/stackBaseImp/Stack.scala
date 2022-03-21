f508712f2539b812710c594ffdbc2338ae0a2878
SoniRat <Soniesen.Ratnasingam@htwg-konstanz.de>
2022-03-21 13:11:00 +0100

    enum finished

--- src/main/scala/UNO/model/stackComponent/stackBaseImp/Stack.scala
+++ src/main/scala/UNO/model/stackComponent/stackBaseImp/Stack.scala
@@ -18,16 +18,16 @@ case class Stack(stackCards: List[Card]) extends StackInterface:
     val green = List(Card("0", Color.Green.name), Card("1", Color.Green.name), Card("2", Color.Green.name), Card("3", Color.Green.name),
       Card("4", Color.Green.name), Card("5", Color.Green.name), Card("6", Color.Green.name), Card("7", Color.Green.name), Card("8", Color.Green.name),
       Card("9", Color.Green.name), Card("+2", Color.Green.name), Card("<-->", Color.Green.name), Card("Ø", Color.Green.name))
-    val red = List(Card("0", "red"), Card("1", "red"), Card("2", "red"), Card("3", "red"),
-      Card("4", "red"), Card("5", "red"), Card("6", "red"), Card("7", "red"), Card("8", "red"),
-      Card("9", "red"), Card("+2", "red"), Card("<-->", "red"), Card("Ø", "red"))
-    val blue = List(Card("0", "blue"), Card("1", "blue"), Card("2", "blue"), Card("3", "blue"),
-      Card("4", "blue"), Card("5", "blue"), Card("6", "blue"), Card("7", "blue"), Card("8", "blue"),
-      Card("9", "blue"), Card("+2", "blue"), Card("<-->", "blue"), Card("Ø", "blue"))
-    val yellow = List(Card("0", "yellow"), Card("1", "yellow"), Card("2", "yellow"), Card("3", "yellow"),
-      Card("4", "yellow"), Card("5", "yellow"), Card("6", "yellow"), Card("7", "yellow"), Card("8", "yellow"),
-      Card("9", "yellow"), Card("+2", "yellow"), Card("<-->", "yellow"), Card("Ø", "yellow"))
-    val black = List(Card("ColorSwitch", "black"), Card("4+ ColorSwitch", "black"))
+    val red = List(Card("0", Color.Red.name), Card("1", Color.Red.name), Card("2", Color.Red.name), Card("3", Color.Red.name),
+      Card("4", Color.Red.name), Card("5", Color.Red.name), Card("6", Color.Red.name), Card("7", Color.Red.name), Card("8", Color.Red.name),
+      Card("9", Color.Red.name), Card("+2", Color.Red.name), Card("<-->", Color.Red.name), Card("Ø", Color.Red.name))
+    val blue = List(Card("0", Color.Blue.name), Card("1", Color.Blue.name), Card("2", Color.Blue.name), Card("3", Color.Blue.name),
+      Card("4", Color.Blue.name), Card("5", Color.Blue.name), Card("6", Color.Blue.name), Card("7", Color.Blue.name), Card("8", Color.Blue.name),
+      Card("9", Color.Blue.name), Card("+2", Color.Blue.name), Card("<-->", Color.Blue.name), Card("Ø", Color.Blue.name))
+    val yellow = List(Card("0",Color.Yellow.name), Card("1",Color.Yellow.name), Card("2",Color.Yellow.name), Card("3",Color.Yellow.name),
+      Card("4",Color.Yellow.name), Card("5",Color.Yellow.name), Card("6",Color.Yellow.name), Card("7",Color.Yellow.name), Card("8",Color.Yellow.name),
+      Card("9",Color.Yellow.name), Card("+2",Color.Yellow.name), Card("<-->",Color.Yellow.name), Card("Ø",Color.Yellow.name))
+    val black = List(Card("ColorSwitch",Color.Black.name), Card("4+ ColorSwitch",Color.Black.name))
     copy(green ::: green ::: red ::: red ::: blue ::: blue ::: yellow ::: yellow ::: black ::: black ::: Nil)
 
   def shuffleCards(): Stack =
