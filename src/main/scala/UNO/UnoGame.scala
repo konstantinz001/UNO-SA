8647a354be4009de38f79ceeb2495a739ec80881
Konstantin Zabaznov <konsi.zabaznov@web.de>
2022-03-22 17:51:25 +0100

    Merge Project and Tests

--- src/main/scala/UNO/UnoGame.scala
+++ src/main/scala/UNO/UnoGame.scala
@@ -14,21 +14,19 @@ object UnoGame:
   val injector = Guice.createInjector(new UnoGameModule)
   val Controller = injector.getInstance(classOf[controllerInterface])
   val tui = TUI(Controller)
-
-  var UIType: Boolean = if System.getenv("UI_TYPE").equals("gui") then true else false
+  val UIType: Boolean = if System.getenv("UI_TYPE").equals("gui") then true else false
 
   @main def main(): Unit =
 
-    print(State.handle(instructionEvent()))
-    print(State.handle(gameStatsEvent()))
+  print(State.handle(instructionEvent()))
+  print(State.handle(gameStatsEvent()))
 
-    if (UIType == true) then
-      val gui = SwingGui(Controller)
-
-    var input1: String = ""
-    while input1 != "q"
-    do
-      input1 = readLine("\nInstruction: ")
-      print(tui.processInputLine(input1))
-      if input1 == "q" then
-        System.exit(0)
+  if (UIType == true) then
+    val gui = SwingGui(Controller)
+  var input1: String = ""
+  while input1 != "q"
+  do
+    input1 = readLine("\nInstruction: ")
+    print(tui.processInputLine(input1))
+    if input1 == "q" then
+      System.exit(0)
