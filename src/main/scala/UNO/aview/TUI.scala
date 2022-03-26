14bdfe8e75c8cb60f152a71151b7b00b266f478c
SoniRat <Soniesen.Ratnasingam@htwg-konstanz.de>
2022-03-26 12:27:59 +0100

    starting to use short classes

--- src/main/scala/UNO/aview/TUI.scala
+++ src/main/scala/UNO/aview/TUI.scala
@@ -1,14 +1,14 @@
 package UNO.aview
 
 import UNO.controller.controllerComponent.controllerBaseImp.{updateStates}
-import UNO.util.{State, Strategy, callFirstUnoEvent, callSecondUnoEvent, exitGameEvent, forgotCallUnoEvent, gameStatsEvent, removeCardEvent, removeFalseCardEvent, removePlayerCardEvent, setPlayerCardEvent, toManyCardsEvent}
+//import UNO.util.{State, Strategy, callFirstUnoEvent, callSecondUnoEvent, exitGameEvent, forgotCallUnoEvent, gameStatsEvent, removeCardEvent, removeFalseCardEvent, removePlayerCardEvent, setPlayerCardEvent, toManyCardsEvent}
+import UNO.util.*
 import UNO.controller.controllerComponent.controllerInterface
 
 import scala.swing.Reactor
 
 def print1: Unit =
   print(State.handle(gameStatsEvent()))
-
 
 
 class TUI(controller: controllerInterface) extends Reactor:
@@ -19,36 +19,13 @@ class TUI(controller: controllerInterface) extends Reactor:
 
     val is: Array[String] = input.split(" ")
     is(0) match
-
       case "s" =>
         controller.getCard()
         State.handle(setPlayerCardEvent())
       case "r" =>
-        if controller.playerList(0).playerCards(is(1).toInt).color.equals("black") then
-          controller.colorSet = is(2)
-        if Strategy.handle(removeCardEvent(is(1).toInt), is(1).toInt) && controller.playerList(0).playerCards.size >= 3 then
-          controller.removeCard(is(1).toInt)
-          State.handle(removePlayerCardEvent(is(1).toInt), is(1).toInt)
-        else if !Strategy.handle(removeCardEvent(is(1).toInt), is(1).toInt) && controller.playerList(0).playerCards.size >= 3 then
-          State.handle(removeFalseCardEvent())
-        else
-          controller.removeCard(is(1).toInt)
-          controller.getCard()
-          controller.playerList = controller.playerList.reverse
-          controller.getCard()
-          State.handle(forgotCallUnoEvent())
+        case_r(is)
       case "u" =>
-        controller.unoCall = true
-        if controller.playerList(0).playerCards.size.equals(2) then
-          controller.removeCard(is(1).toInt)
-          State.handle(callFirstUnoEvent(is(1).toInt), is(1).toInt)
-        else if controller.playerList(0).playerCards.size.equals(1) then
-          State.handle(callSecondUnoEvent())
-        else
-          controller.getCard()
-          controller.playerList = controller.playerList.reverse
-          controller.getCard()
-          State.handle(toManyCardsEvent())
+        case_u(is)
       case "q" =>
         State.handle(exitGameEvent())
 
@@ -70,4 +47,33 @@ class TUI(controller: controllerInterface) extends Reactor:
   reactions += {
     case event: updateStates => print1
   }
+  def case_r(is:Array[String]):String=
+    if controller.playerList(0).playerCards(is(1).toInt).color.equals("black") then
+      controller.colorSet = is(2)
+    if Strategy.handle(removeCardEvent(is(1).toInt), is(1).toInt) && controller.playerList(0).playerCards.size >= 3 then
+      controller.removeCard(is(1).toInt)
+      State.handle(removePlayerCardEvent(is(1).toInt), is(1).toInt)
+    else if !Strategy.handle(removeCardEvent(is(1).toInt), is(1).toInt) && controller.playerList(0).playerCards.size >= 3 then
+      State.handle(removeFalseCardEvent())
+    else
+      controller.removeCard(is(1).toInt)
+      controller.getCard()
+      controller.playerList = controller.playerList.reverse
+      controller.getCard()
+      State.handle(forgotCallUnoEvent())
+
+
+  def case_u(is:Array[String]):String =
+    controller.unoCall = true
+    if controller.playerList(0).playerCards.size.equals(2) then
+      controller.removeCard(is(1).toInt)
+      State.handle(callFirstUnoEvent(is(1).toInt), is(1).toInt)
+    else if controller.playerList(0).playerCards.size.equals(1) then
+      State.handle(callSecondUnoEvent())
+    else
+      controller.getCard()
+      controller.playerList = controller.playerList.reverse
+      controller.getCard()
+      State.handle(toManyCardsEvent())
+
 
