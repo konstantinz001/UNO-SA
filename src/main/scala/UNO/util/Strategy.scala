24d2129815d9bfbce848f64b409579ff3db07e58
SoniRat <Soniesen.Ratnasingam@htwg-konstanz.de>
2022-04-05 11:52:49 +0200

    safe state

--- src/main/scala/UNO/util/Strategy.scala
+++ src/main/scala/UNO/util/Strategy.scala
@@ -1,15 +1,11 @@
 package UNO.util
 
 object Strategy:
-  // closure
+
   var strategy = false
 
-/*  def handle(e: Event2, index: Int): Boolean =
-    e match
-      case a: removeCardEvent => strategy = removeCardEvent(index).removeCard
-    strategy*/
-
-  def handle (e:Event2)(index:Int):Boolean =
+  def handle(e: Event2, index: Int): Boolean =
     e match
       case a: removeCardEvent => strategy = removeCardEvent(index).removeCard
     strategy
+
