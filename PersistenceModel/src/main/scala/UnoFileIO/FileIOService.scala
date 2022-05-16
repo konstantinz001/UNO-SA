1cd98d24b8ddd405e393a7f837fd8f2524048dbf
Konstantin Zabaznov <konsi.zabaznov@web.de>
2022-05-17 01:41:22 +0200

    Changes + Gattling Finished

--- PersistenceModel/src/main/scala/UnoFileIO/FileIOService.scala
+++ PersistenceModel/src/main/scala/UnoFileIO/FileIOService.scala
@@ -5,10 +5,13 @@ import UnoFileIO.fileIOJsonImp.FileIO
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
 import akka.http.scaladsl.server.Directives.{post, *}
 import net.codingwell.scalaguice.InjectorExtensions.ScalaInjector
 import com.google.inject.Guice
+
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 import scala.util.{Failure, Success}
 
@@ -17,8 +20,6 @@ case object FileIOService {
   def main(args: Array[String]): Unit = {
 
     val fileIO = new FileIO
-    //val injector = Guice.createInjector(new FileIOModule)
-    //val db = injector.getInstance(classOf[DaoInterface])
     val db = new FileIODBProxy
     implicit val system = ActorSystem(Behaviors.empty, "my-system")
     implicit val executionContext = system.executionContext
@@ -31,19 +32,19 @@ case object FileIOService {
       concat (
         get {
           path("load") {
-            complete(HttpEntity(ContentTypes.`application/json`, fileIO.load))
+            complete(StatusCodes.Created, HttpEntity(ContentTypes.`application/json`, fileIO.load))
           }
         },
         get {
           path("loadDB") {
-            complete(HttpEntity(ContentTypes.`application/json`, db.load("1")))
+            complete(StatusCodes.Created, HttpEntity(ContentTypes.`application/json`, db.load("1")))
           }
         },
         post {
           path("save") {
             entity(as [String]) { game =>
               fileIO.save(game)
-              complete("game saved")
+              complete(StatusCodes.Accepted,"game saved")
             }
           }
         },
@@ -51,32 +52,17 @@ case object FileIOService {
           path("saveDB") {
             entity(as [String]) { game =>
               db.save(game)
-              complete("game saved")
+              complete(StatusCodes.Accepted,"game saved")
             }
           }
         }
       )
 
-    //val bindingFuture = Http().newServerAt(fileIOUri, fileIOPort).bind(route)
     val bindingFuture = Http().newServerAt(fileIOUri, 8081).bind(route)
-
-    /*
-    bindingFuture.onComplete{
-      case Success(binding) => {
-        val address = binding.localAddress
-        println(s"File IO Save: http://${address.getHostName}:${address.getPort}/${"save"}\n" +
-          s"File IO Load: http://${address.getHostName}:${address.getPort}/${"load"} \n")
-      }
-      case Failure(exception) => {
-        println("File IO REST service couldn't be started! Error: " + exception + "\n")
-      }
-    }
-
 
     def stop():Unit =
       bindingFuture
         .flatMap(_.unbind()) // trigger unbinding from the port
         .onComplete(_ => system.terminate()) // and shutdown when done
-    */
   }
 }
